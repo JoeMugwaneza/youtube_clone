b244639f0ca8337e96d6432ec38fdd332dfc4ac1
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-09-20 22:31:00 +0200

    ntago biba byoroshye

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 
 import App from './app';
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<app/>)
