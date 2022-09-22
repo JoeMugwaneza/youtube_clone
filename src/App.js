5f1cf8932481420534f1ef2a1bc1b17c8e4ab0bb
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-09-22 04:56:55 +0200

    put all imported components in one index.js and exports altogether

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
+import {Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
+
 
 export default function App(){
   return (
