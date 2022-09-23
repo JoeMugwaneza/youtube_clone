943e5ee8910046114df05e9b743910cb3367daba
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-09-23 06:04:48 +0200

    starting working on typograph

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,8 +8,8 @@ const Feed = () => {
     >
       <Box sx={{height: {sx: 'auto', md: '92vh'}, borderRight: '1px solid #3d3d3d', px: {sx: 0, md: 2}}}>
         Sidebar
-        <Typography>
-          
+        <Typography className="copyright" variant="body2" sx={{mt: 1.5, color: "#fff"}}>
+          Copyright 2022 Joseph Mugwaneza
         </Typography>
       </Box>
     </Stack>
