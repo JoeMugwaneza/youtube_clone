903c2e503da7f265e5e967f613eef3111e5e9591
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-09-24 22:22:05 +0200

    worked on changing the category as clicked, now trying to render category videos as clicked

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,6 +5,15 @@ import Videos from './Videos';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState('New');
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items),
+    );
+  }, [selectedCategory]);
+
   return (
     <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
       <Box
@@ -14,7 +23,10 @@ const Feed = () => {
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
         <Typography
           className='copyright'
           variant='body2'
@@ -24,18 +36,16 @@ const Feed = () => {
         </Typography>
       </Box>
       <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
-
-
         <Typography
           variant='h4'
           fontWeight='bold'
           mb={2}
           sx={{ color: 'white' }}
         >
-          New <span style={{ color: '#F31503' }}>Videos</span>
+          {selectedCategory} <span style={{ color: '#F31503' }}>Videos</span>
         </Typography>
 
-        <Videos Videos={[]} />
+        <Videos Videos={videos} />
       </Box>
     </Stack>
   );
