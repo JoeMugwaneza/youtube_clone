3d950cdea8eb94ccc7dad33ada2dead41cd898ce
Mugwaneza Joseph <muganezajoseph@gmail.com>
2022-10-04 05:43:10 +0200

    failing to fix processing .env key

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -2,12 +2,14 @@ import axios from "axios";
 
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 const options = {
     params: {
       maxResults: '50',
     },
     headers: {
-      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_API,
+      'X-RapidAPI-Key': API_KEY,
       'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
@@ -15,4 +17,4 @@ const options = {
   export const fetchFromAPI = async (url) =>{
    const {data}  = await axios.get(`${BASE_URL}/${url}`, options)
     return data;
-  }
+  };
